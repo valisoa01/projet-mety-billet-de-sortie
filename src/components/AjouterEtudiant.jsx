4ae052a9ffb1b18af5302fe05ab36d866374e7cb
anasta456 <manarintsoaanastabreney@gmail.com>
2024-09-12 15:55:54 +0300

    Responvise

--- src/components/AjouterEtudiant.jsx
+++ src/components/AjouterEtudiant.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { db } from '../config/firebase-config';
-import { collection, addDoc, onSnapshot, query, orderBy } from 'firebase/firestore';
-import '../styles/AjouterEtudiant.css';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { db } from "../config/firebase-config";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import "../styles/AjouterEtudiant.css";
+import { NavLink } from "react-router-dom";
 
 function AjouterEtudiant() {
-  const [identifiant, setIdentifiant] = useState('');
-  const [nomEleve, setNomEleve] = useState('');
-  const [email, setEmail] = useState('');
-  const [tel, setTel] = useState('');
+  const [identifiant, setIdentifiant] = useState("");
+  const [nomEleve, setNomEleve] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
   const [showForm, setShowForm] = useState(true);
   const [students, setStudents] = useState([]);
 
@@ -17,29 +23,32 @@ function AjouterEtudiant() {
 
     try {
       // Ajoutez le nouvel étudiant avec l'Identifiant manuel
-      await addDoc(collection(db, 'EtudiantTab'), {
-        identifiant,  // Stocke l'identifiant manuellement saisi
+      await addDoc(collection(db, "EtudiantTab"), {
+        identifiant, // Stocke l'identifiant manuellement saisi
         email,
         nomEleve,
         tel,
       });
 
       // Réinitialisez le formulaire
-      setIdentifiant('');
-      setNomEleve('');
-      setEmail('');
-      setTel('');
+      setIdentifiant("");
+      setNomEleve("");
+      setEmail("");
+      setTel("");
 
-      alert('Étudiant ajouté avec succès !');
+      alert("Étudiant ajouté avec succès !");
     } catch (error) {
-      console.error('Erreur lors de l\'ajout de l\'étudiant :', error);
-      alert('Erreur lors de l\'ajout de l\'étudiant.');
+      console.error("Erreur lors de l'ajout de l'étudiant :", error);
+      alert("Erreur lors de l'ajout de l'étudiant.");
     }
   };
 
   useEffect(() => {
     // Observer les changements dans la collection EtudiantTab
-    const q = query(collection(db, 'EtudiantTab'), orderBy('identifiant', 'asc'));
+    const q = query(
+      collection(db, "EtudiantTab"),
+      orderBy("identifiant", "asc")
+    );
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const studentsList = [];
       querySnapshot.forEach((doc) => {
@@ -55,13 +64,12 @@ function AjouterEtudiant() {
   return (
     <>
       <NavLink to="/Connections/AdminHome">
-        <button     className="back-btn5">Retour</button>
+        <button className="back-btn5">Retour</button>
       </NavLink>
 
-     
-        {showForm ? (
-          <>
-           <div className="form-container">
+      {showForm ? (
+        <>
+          <div className="form-container">
             <h2>Ajouter un étudiant</h2>
             <form onSubmit={handleSubmit}>
               <div className="form-group1">
@@ -100,14 +108,18 @@ function AjouterEtudiant() {
                   required
                 />
               </div>
-              <button className='subm' type="submit">Ajouter</button>
+              <button className="subm" type="submit">
+                Ajouter
+              </button>
             </form>
-            </div>
-          </>
-        ) : (
-          <>
-           <div className="form-container1">
-            <button onClick={toggleForm} className='button'>Ajouter étudiant</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="form-container1">
+            <button onClick={toggleForm} className="button">
+              Ajouter étudiant
+            </button>
             <div className="studen-table-container1">
               <h2>Liste des étudiants</h2>
               <table className="studen-table">
@@ -131,10 +143,12 @@ function AjouterEtudiant() {
                 </tbody>
               </table>
             </div>
-            </div>
-          </>
-        )}
-        <button className='butt' onClick={toggleForm}>{showForm ? 'Afficher les élèves' : 'Ajouter étudiant'}</button>
+          </div>
+        </>
+      )}
+      <button className="butt" onClick={toggleForm}>
+        {showForm ? "Afficher les élèves" : "Ajouter étudiant"}
+      </button>
     </>
   );
 }
